5796dda16bc4a7133875330a8701ae0f6d0cd37f
Anatoly Khaytovich <anatolyuss@gmail.com>
2015-12-16 23:35:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,25 +11,22 @@ var fmtp = require('./migration/fmtp/FromMySQL2PostgreSQL');
 var nmig = new fmtp.FromMySQL2PostgreSQL();
 console.log();
 
-if (process.argv.length < 3) {
-    console.log('\t--Cannot run migration due to missing command line arguments');
-} else {
-    fs.readFile(process.argv[2], function(error, data) {
-        var errMsg;
-        
-        if (error) {
-            errMsg = '\t--Cannot run migration\nCannot read configuration info from ' + process.argv[2];
+fs.readFile(__dirname + '/config.json', function(error, data) {
+    var errMsg;
+    
+    if (error) {
+        errMsg = '\t--Cannot run migration\nCannot read configuration info from ' + __dirname + '/config.json';
+        console.log(errMsg);
+    } else {
+        try {
+            var config         = JSON.parse(data.toString());
+            config.tempDirPath = __dirname + '/temporary_directory';
+            config.logsDirPath = __dirname + '/logs_directory';
+            nmig.run(config);
+        } catch (err) {
+            errMsg = '\t--Cannot parse JSON from' + __dirname + '/config.json';
             console.log(errMsg);
-        } else {
-            try {
-                var config         = JSON.parse(data.toString());
-                config.tempDirPath = __dirname + '/temporary_directory';
-                config.logsDirPath = __dirname + '/logs_directory';
-                nmig.run(config);
-            } catch (err) {
-                errMsg = '\t--Cannot parse JSON from' + process.argv[2];
-                console.log(errMsg);
-            }
         }
-    });
-}
+    }
+});
+
