670bf210ef54304bb1992ac4e6c049a3f3ed8103
Anatoly Khaytovich <anatolyuss@gmail.com>
2015-12-17 19:41:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,24 +9,19 @@
 var fs   = require('fs');
 var fmtp = require('./migration/fmtp/FromMySQL2PostgreSQL');
 var nmig = new fmtp.FromMySQL2PostgreSQL();
-console.log();
 
 fs.readFile(__dirname + '/config.json', function(error, data) {
-    var errMsg;
-    
     if (error) {
-        errMsg = '\t--Cannot run migration\nCannot read configuration info from ' + __dirname + '/config.json';
-        console.log(errMsg);
+        console.log('\n\t--Cannot run migration\nCannot read configuration info from ' + __dirname + '/config.json');
     } else {
         try {
-            var config         = JSON.parse(data.toString());
-            config.tempDirPath = __dirname + '/temporary_directory';
-            config.logsDirPath = __dirname + '/logs_directory';
+            var config              = JSON.parse(data.toString());
+            config.tempDirPath      = __dirname + '/temporary_directory';
+            config.logsDirPath      = __dirname + '/logs_directory';
+			config.dataTypesMapAddr = __dirname + '/DataTypesMap.json'; 
             nmig.run(config);
         } catch (err) {
-            errMsg = '\t--Cannot parse JSON from' + __dirname + '/config.json';
-            console.log(errMsg);
+            console.log('\n\t--Cannot parse JSON from' + __dirname + '/config.json');
         }
     }
 });
-
