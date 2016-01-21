9548c08b9848c338259c330c97708d6a3c52077e
Anatoly Khaytovich <anatolyuss@gmail.com>
2016-01-21 19:10:33 +0200

    Update ViewGenerator.js

--- migration/fmtp/ViewGenerator.js
+++ migration/fmtp/ViewGenerator.js
@@ -6,9 +6,6 @@
  * @author Anatoly Khaytovich <anatolyuss@gmail.com>
  */
 'use strict';
-const fs    = require('fs');
-const mysql = require('mysql');
-const pg    = require('pg');
 
 /**
  * Constructor.
@@ -18,16 +15,30 @@ function ViewGenerator() {
 }
 
 /**
- * Attempts to convert mysql view to postgresql view.
+ * Attempts to convert MySQL view to PostgreSQL view.
  *
  * @param   {String} schema
  * @param   {String} viewName
- * @returns {Promise}
+ * @param   {String} mysqlViewCode
+ * @returns {String}
  */
-ViewGenerator.prototype.generateView = function(schema, viewName) {
-    return new Promise(resolve => {
-        resolve();
-    });
+ViewGenerator.prototype.generateView = function(schema, viewName, mysqlViewCode) {
+    mysqlViewCode        = mysqlViewCode.split('`').join('"');
+    let queryStart       = mysqlViewCode.indexOf('AS');
+    mysqlViewCode        = mysqlViewCode.slice(queryStart);
+    let arrMysqlViewCode = mysqlViewCode.split(' ');
+
+    for (let i = 0; i < arrMysqlViewCode.length; i++) {
+        if (
+            arrMysqlViewCode[i].toLowerCase() === 'from'
+            || arrMysqlViewCode[i].toLowerCase() === 'join'
+            && i + 1 < arrMysqlViewCode.length
+        ) {
+            arrMysqlViewCode[i + 1] = '"' + schema + '".' + arrMysqlViewCode[i + 1];
+        }
+    }
+
+    return 'CREATE OR REPLACE VIEW "' + schema + '"."' + viewName + '" ' + arrMysqlViewCode.join(' ') + ';';
 };
 
 module.exports.ViewGenerator = ViewGenerator;
