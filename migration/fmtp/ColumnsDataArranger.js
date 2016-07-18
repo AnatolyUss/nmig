23e25a35c11921dade7e94ddabf98256bda2b826
Anatoly Khaytovich <anatolyuss@gmail.com>
2016-07-18 07:56:27 +0300

    Update ColumnsDataArranger.js

--- migration/fmtp/ColumnsDataArranger.js
+++ migration/fmtp/ColumnsDataArranger.js
@@ -50,9 +50,9 @@ module.exports = function(arrTableColumns) {
             arrTableColumns[i].Type.indexOf('timestamp') !== -1
             || arrTableColumns[i].Type.indexOf('date') !== -1
         ) {
-            strRetVal += 'IF(`' + arrTableColumns[i].Field
-                      +  '` IN(\'0000-00-00\', \'0000-00-00 00:00:00\'), \'-INFINITY\', `'
-                      +  arrTableColumns[i].Field + '`),';
+            strRetVal += 'IF(\`' + arrTableColumns[i].Field
+                      +  '` IN(\'0000-00-00\', \'0000-00-00 00:00:00\'), \'-INFINITY\', CAST(`'
+                      +  arrTableColumns[i].Field + '` AS CHAR)),';
         } else {
             strRetVal += '`' + arrTableColumns[i].Field + '`,';
         }
