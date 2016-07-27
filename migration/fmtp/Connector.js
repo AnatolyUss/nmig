cab9f58c355ed9ed1105dda3678a015e39f799c6
Anatoly Khaytovich <anatolyuss@gmail.com>
2016-07-27 22:39:35 +0300

    Create Connector.js

---
+++ migration/fmtp/Connector.js
@@ -0,0 +1,50 @@
+/*
+ * This file is a part of "NMIG" - the database migration tool.
+ *
+ * Copyright 2016 Anatoly Khaytovich <anatolyuss@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (please see the "LICENSE.md" file).
+ * If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ *
+ * @author Anatoly Khaytovich <anatolyuss@gmail.com>
+ */
+'use strict';
+
+const mysql = require('mysql');
+const log   = require('./Logger');
+
+/**
+ * Check if both servers are connected.
+ * If not, than create connections.
+ *
+ * @param   {Conversion} self
+ * @returns {Promise}
+ */
+module.exports = function(self) {
+    return new Promise((resolve, reject) => {
+        if (!self._mysql) {
+            self._sourceConString.connectionLimit = self._maxPoolSizeSource;
+            let pool                              = mysql.createPool(self._sourceConString);
+
+            if (pool) {
+                self._mysql = pool;
+                resolve();
+            } else {
+                log(self, '\t--[connect] Cannot connect to MySQL server...');
+                reject();
+            }
+        } else {
+            resolve();
+        }
+    });
+};
