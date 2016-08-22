8f5b2fa764fcad72cb142d60ec3172346e0dd1b2
Anatoly Khaytovich <anatolyuss@gmail.com>
2016-08-22 07:51:30 +0300

    Added 'loader_max_old_space_size' parameter

--- migration/fmtp/Conversion.js
+++ migration/fmtp/Conversion.js
@@ -28,56 +28,56 @@ const isIntNumeric = require('./IntegerValidator');
  * @param {Object} config
  */
 module.exports = function Conversion(config) {
-    this._config              = config;
-    this._sourceConString     = this._config.source;
-    this._targetConString     = this._config.target;
-    this._tempDirPath         = this._config.tempDirPath;
-    this._logsDirPath         = this._config.logsDirPath;
-    this._dataTypesMapAddr    = __dirname + '/DataTypesMap.json';
-    this._allLogsPath         = this._logsDirPath + '/all.log';
-    this._errorLogsPath       = this._logsDirPath + '/errors-only.log';
-    this._notCreatedViewsPath = this._logsDirPath + '/not_created_views';
-    this._copyOnly            = this._config.copy_only;
-    this._noVacuum            = this._config.no_vacuum;
-    this._excludeTables       = this._config.exclude_tables;
-    this._timeBegin           = new Date();
-    this._encoding            = this._config.encoding === undefined ? 'utf8' : this._config.encoding;
-    this._dataChunkSize       = this._config.data_chunk_size === undefined ? 1 : +this._config.data_chunk_size;
-    this._dataChunkSize       = this._dataChunkSize < 1 ? 1 : this._dataChunkSize;
-    this._0777                = '0777';
-    this._mysql               = null;
-    this._tablesToMigrate     = [];
-    this._viewsToMigrate      = [];
-    this._tablesCnt           = 0;
-    this._viewsCnt            = 0;
-    this._dataPool            = [];
-    this._dicTables           = Object.create(null);
-    this._mySqlDbName         = this._sourceConString.database;
-    this._schema              = this._config.schema === undefined ||
-                                this._config.schema === ''
-                                ? this._mySqlDbName
-                                : this._config.schema;
+    this._config                = config;
+    this._sourceConString       = this._config.source;
+    this._targetConString       = this._config.target;
+    this._tempDirPath           = this._config.tempDirPath;
+    this._logsDirPath           = this._config.logsDirPath;
+    this._dataTypesMapAddr      = __dirname + '/DataTypesMap.json';
+    this._allLogsPath           = this._logsDirPath + '/all.log';
+    this._errorLogsPath         = this._logsDirPath + '/errors-only.log';
+    this._notCreatedViewsPath   = this._logsDirPath + '/not_created_views';
+    this._copyOnly              = this._config.copy_only;
+    this._noVacuum              = this._config.no_vacuum;
+    this._excludeTables         = this._config.exclude_tables;
+    this._timeBegin             = new Date();
+    this._encoding              = this._config.encoding === undefined ? 'utf8' : this._config.encoding;
+    this._dataChunkSize         = this._config.data_chunk_size === undefined ? 1 : +this._config.data_chunk_size;
+    this._dataChunkSize         = this._dataChunkSize < 1 ? 1 : this._dataChunkSize;
+    this._0777                  = '0777';
+    this._mysql                 = null;
+    this._pg                    = null;
+    this._tablesToMigrate       = [];
+    this._viewsToMigrate        = [];
+    this._tablesCnt             = 0;
+    this._viewsCnt              = 0;
+    this._dataPool              = [];
+    this._dicTables             = Object.create(null);
+    this._mySqlDbName           = this._sourceConString.database;
+    this._schema                = this._config.schema === undefined ||
+                                  this._config.schema === ''
+                                  ? this._mySqlDbName
+                                  : this._config.schema;
 
-    this._maxPoolSizeSource   = this._config.max_pool_size_source !== undefined &&
-                                isIntNumeric(this._config.max_pool_size_source)
-                                ? +this._config.max_pool_size_source
-                                : 10;
+    this._maxPoolSizeSource     = this._config.max_pool_size_source !== undefined &&
+                                  isIntNumeric(this._config.max_pool_size_source)
+                                  ? +this._config.max_pool_size_source
+                                  : 10;
 
-    this._maxPoolSizeTarget   = this._config.max_pool_size_target !== undefined &&
-                                isIntNumeric(this._config.max_pool_size_target)
-                                ? +this._config.max_pool_size_target
-                                : 10;
+    this._maxPoolSizeTarget     = this._config.max_pool_size_target !== undefined &&
+                                  isIntNumeric(this._config.max_pool_size_target)
+                                  ? +this._config.max_pool_size_target
+                                  : 10;
 
-    this._maxPoolSizeSource   = this._maxPoolSizeSource > 0 ? this._maxPoolSizeSource : 10;
-    this._maxPoolSizeTarget   = this._maxPoolSizeTarget > 0 ? this._maxPoolSizeTarget : 10;
+    this._maxPoolSizeSource     = this._maxPoolSizeSource > 0 ? this._maxPoolSizeSource : 10;
+    this._maxPoolSizeTarget     = this._maxPoolSizeTarget > 0 ? this._maxPoolSizeTarget : 10;
 
-    this._pipeWidth           = this._config.pipe_width !== undefined &&
-                                isIntNumeric(this._config.pipe_width)
-                                ? Math.abs(+this._config.pipe_width)
-                                : this._maxPoolSizeTarget;
+    this._pipeWidth             = this._config.pipe_width !== undefined &&
+                                  isIntNumeric(this._config.pipe_width)
+                                  ? +this._config.pipe_width
+                                  : this._maxPoolSizeTarget;
 
-    this._pipeWidth           = this._pipeWidth > this._maxPoolSizeTarget ? this._maxPoolSizeTarget : this._pipeWidth;
-    this._targetConString     = 'postgresql://' + this._targetConString.user + ':' + this._targetConString.password
-                                + '@' + this._targetConString.host + ':' + this._targetConString.port + '/'
-                                + this._targetConString.database + '?client_encoding=' + this._targetConString.charset;
+    this._pipeWidth             = this._pipeWidth > this._maxPoolSizeTarget ? this._maxPoolSizeTarget : this._pipeWidth;
+    this._loaderMaxOldSpaceSize = this._config.loader_max_old_space_size;
+    this._loaderMaxOldSpaceSize = isIntNumeric(this._loaderMaxOldSpaceSize) ? this._loaderMaxOldSpaceSize : 'DEFAULT';
 };
