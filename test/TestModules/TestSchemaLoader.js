c0e3dcbdbb8988a205114a08f5be9d911e66c402
Anatoly <anatolyuss@gmail.com>
2018-01-30 01:25:57 +0200

    added readFile(), readTestSchema() and readTestBlob() methods

--- test/TestModules/TestSchemaLoader.js
+++ test/TestModules/TestSchemaLoader.js
@@ -151,23 +151,43 @@ module.exports = class TestSchemaLoader {
     }
 
     /**
+     * Reads contents from the specified resource.
+     *
+     * @param {String} filePath
+     *
+     * @returns {Promise<Buffer>}
+     */
+    readFile(filePath) {
+        return new Promise(resolve => {
+            fs.readFile(filePath, (error, data) => {
+                if (error) {
+                    console.log(`\t--[readFile] Cannot read file from ${ filePath }`);
+                    process.exit();
+                }
+
+                resolve(data);
+            });
+        });
+    }
+
+    /**
      * Reads test schema sql file.
      *
      * @returns {Promise<Buffer>}
      */
     readTestSchema() {
-        return new Promise(resolve => {
-            const testSchemaFilePath = path.join(__dirname, '..', 'test_schema.sql');
+        const testSchemaFilePath = path.join(__dirname, '..', 'test_schema.sql');
+        return this.readFile(testSchemaFilePath);
+    }
 
-            fs.readFile(testSchemaFilePath, (error, sql) => {
-                if (error) {
-                    console.log(`\t--[readTestSchema] Cannot read test schema from ${ testSchemaFilePath }`);
-                    process.exit();
-                }
-
-                resolve(sql);
-            });
-        });
+    /**
+     * Reads an image for a sake of the blob testing.
+     *
+     * @returns {Promise<Buffer>}
+     */
+    readTestBlob() {
+        const blobPath = path.join(__dirname, '..', 'TestAssets', 'test.png');
+        return this.readFile(blobPath);
     }
 
     /**
@@ -179,7 +199,7 @@ module.exports = class TestSchemaLoader {
      */
     loadTestSchema(conversion) {
         return connect(conversion)
-            .then(this.readTestSchema)
+            .then(this.readTestSchema.bind(this))
             .then(sqlBuffer => {
                 return new Promise(resolve => {
                     conversion._mysql.getConnection((error, connection) => {
@@ -209,52 +229,54 @@ module.exports = class TestSchemaLoader {
      * @returns {Promise<Conversion>}
      */
     loadTestData(conversion) {
-        return connect(conversion).then(() => {
-            return new Promise(resolve => {
-                conversion._mysql.getConnection((error, connection) => {
-                    if (error) {
-                        this.processFatalError(conversion, error);
-                    }
-
-                    const insertParams = {
-                        id_test_unique_index             : 7384,
-                        id_test_composite_unique_index_1 : 125,
-                        id_test_composite_unique_index_2 : 234,
-                        id_test_index                    : 123,
-                        int_test_not_null                : 123,
-                        id_test_composite_index_1        : 11,
-                        id_test_composite_index_2        : 22,
-                        json_test_comment                : '{"prop1":"First","prop2":2}',
-                        bit                              : 1,
-                        year                             : 1984,
-                        bigint                           : 1234567890123456789,
-                        float                            : 12345.56,
-                        double                           : 123456789.23,
-                        numeric                          : 1234567890,
-                        decimal                          : 1234567890,
-                        char_5                           : 'fghij',
-                        varchar_5                        : 'abcde',
-                        date                             : '1984-07-30',
-                        time                             : '21:12:33',
-                        timestamp                        : '2018-01-01 22:21:20',
-                        enum                             : 'e1',
-                        set                              : 's2',
-                        text                             : 'Test text',
-                        blob                             : '',
-                    };
-
-                    connection.query('INSERT INTO `table_a` SET ?;', insertParams, err => {
-                        connection.release();
-
-                        if (err) {
-                            this.processFatalError(conversion, err);
+        return connect(conversion)
+            .then(this.readTestBlob.bind(this))
+            .then(blobBuffer => {
+                return new Promise(resolve => {
+                    conversion._mysql.getConnection((error, connection) => {
+                        if (error) {
+                            this.processFatalError(conversion, error);
                         }
 
-                        resolve(conversion);
+                        const insertParams = {
+                            id_test_unique_index             : 7384,
+                            id_test_composite_unique_index_1 : 125,
+                            id_test_composite_unique_index_2 : 234,
+                            id_test_index                    : 123,
+                            int_test_not_null                : 123,
+                            id_test_composite_index_1        : 11,
+                            id_test_composite_index_2        : 22,
+                            json_test_comment                : '{"prop1":"First","prop2":2}',
+                            bit                              : 1,
+                            year                             : 1984,
+                            bigint                           : 1234567890123456789,
+                            float                            : 12345.56,
+                            double                           : 123456789.23,
+                            numeric                          : 1234567890,
+                            decimal                          : 1234567890,
+                            char_5                           : 'fghij',
+                            varchar_5                        : 'abcde',
+                            date                             : '1984-07-30',
+                            time                             : '21:12:33',
+                            timestamp                        : '2018-01-01 22:21:20',
+                            enum                             : 'e1',
+                            set                              : 's2',
+                            text                             : 'Test text',
+                            blob                             : blobBuffer,
+                        };
+
+                        connection.query('INSERT INTO `table_a` SET ?;', insertParams, err => {
+                            connection.release();
+
+                            if (err) {
+                                this.processFatalError(conversion, err);
+                            }
+
+                            resolve(conversion);
+                        });
                     });
                 });
             });
-        });
     }
 
     /**
