5dc78ff69ba245ca14e504b8963f8d8699023395
Anatoly <anatolyuss@gmail.com>
2018-01-31 02:40:45 +0200

    added _eventEmitter class attribute

--- src/Classes/Conversion.js
+++ src/Classes/Conversion.js
@@ -63,6 +63,7 @@ module.exports = class Conversion {
             : 10;
 
         this._runsInTestMode          = false;
+        this._eventEmitter            = null;
         this._maxDbConnectionPoolSize = this._maxDbConnectionPoolSize > 0 ? this._maxDbConnectionPoolSize : 10;
         this._loaderMaxOldSpaceSize   = this._config.loader_max_old_space_size;
         this._loaderMaxOldSpaceSize   = this.isIntNumeric(this._loaderMaxOldSpaceSize) ? this._loaderMaxOldSpaceSize : 'DEFAULT';
